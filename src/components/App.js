9eb23f694aab396bf21a5da83815725ec70be071
Johan Ruokangas <johan@latehours.net>
2015-10-21 17:17:07 +0300

    l

--- src/components/App.js
+++ src/components/App.js
@@ -18,11 +18,22 @@ export default class App extends React.Component {
     api.getBoards().then(data => this.setState({ boards: data }));
   }
 
+  addNewBoard(e) {
+    const { boards } = this.state;
+    this.setState({
+      boards: boards.push({
+        name: 'New list',
+        items: ['new new', 'tussi']
+      })
+    });
+  }
+
   render() {
     const { boards } = this.state;
     return (
       <div>
         <h1>Trollo</h1>
+        <button onClick={::this.addNewBoard}>Add new board</button>
 
         {this.props.children && React.cloneElement(
           this.props.children,
